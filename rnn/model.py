6fdf0c85fed4a9979da4bec733e7930dd8ea71a0
iczero <iczero4@gmail.com>
2024-03-14 22:23:45 -0700

    more wip

--- rnn/model.py
+++ rnn/model.py
@@ -1,5 +1,4 @@
 import dataclasses
-import functools
 from dataclasses import dataclass
 from collections.abc import Callable
 
@@ -72,6 +71,50 @@ class ModelConfig:
             qkv_bias=True,
         )
 
+class PartialCrossAttention(nn.Module):
+    "Partial cross attention layer"
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.n_attention_heads = config.n_attention_heads
+        self.n_embed = config.n_embed
+        self.attn_dropout_p = config.attn_dropout_p
+        # in: external: (batch, seq, n_embed), internal: (batch, seq, n_embed)
+        # split these two since q and k/v have different inputs
+        self.q_linear = nn.Linear(
+            config.n_embed,
+            config.n_attention_heads * config.n_embed,
+            bias=config.qkv_bias,
+        )
+        self.kv_linear = nn.Linear(
+            config.n_embed,
+            2 * config.n_attention_heads * config.n_embed,
+            bias=config.qkv_bias,
+        )
+
+    def forward(self, external: torch.Tensor, internal: torch.Tensor):
+        # external is the other sequence concatenated to k/v, internal is our own sequence
+        # q shape: (batch, seq, n_heads, n_embed)
+        q = self.q_linear(internal).unflatten(-1, (self.n_attention_heads, self.n_embed))
+        kv_seq = torch.concat(external, internal, dim=1)
+        # kv_merged shape: (batch, seq, 2, n_heads, n_embed)
+        kv_merged = self.kv_linear(kv_seq).unflatten(-1, (2, self.n_attention_heads, self.n_embed))
+        # extract from merged
+        k = kv_merged[..., 0, :, :]
+        v = kv_merged[..., 1, :, :]
+
+        # transpose (batch, seq, n_heads, n_embed) -> (batch, n_heads, seq, n_embed) for sdp
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        # why does pylint think this isn't callable?
+        # pylint: disable-next=not-callable
+        attn_out = F.scaled_dot_product_attention(q, k, v, dropout_p=self.attn_dropout_p)
+
+        # transpose back
+        return attn_out.transpose(1, 2)
+
 class GatedFeedForward(nn.Module):
     "Feedforward after attention with residual gating"
 
@@ -102,19 +145,29 @@ class GatedFeedForward(nn.Module):
         return resid * resid_gate.unsqueeze(-1)
 
 class InputLayer(nn.Module):
+    "The input layer of the model"
+
     def __init__(self, config: ModelConfig):
         super().__init__()
         self.attn_dropout_p = config.attn_dropout_p
+        self.short_ctx_len = config.short_ctx_len
         # in: (batch, seq)
         self.input_embedding = nn.Embedding(config.vocab_size, config.n_embed)
 
         self.make_qkv = nn.Sequential(
             self.input_embedding,
-            # (batch, seq, n_embed) -> (batch, seq, 3 * n_heads * n_embed)
-            nn.Linear(config.n_embed, 3 * config.n_heads * config.n_embed, bias=config.qkv_bias),
+            # qkv linear, (batch, seq, n_embed) -> (batch, seq, 3 * n_heads * n_embed)
+            nn.Linear(
+                config.n_embed,
+                3 * config.n_attention_heads * config.n_embed,
+                bias=config.qkv_bias,
+            ),
             # (batch, seq, n_embed) -> (batch, seq, 3, n_heads, n_embed)
-            nn.Unflatten(-1, (3, config.n_heads, config.n_embed))
+            nn.Unflatten(-1, (3, config.n_attention_heads, config.n_embed))
         )
+
+        # positional encoding
+        self.rope = RotaryEncoding(config.n_embed, config.short_ctx_len)
 
         # no residuals in input layer
         # ff in: (batch, seq, n_heads, n_embed) -> (batch, seq, n_heads * n_embed)
@@ -128,7 +181,10 @@ class InputLayer(nn.Module):
             nn.Dropout(config.ff_dropout_p),
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        # ensure sequence is of correct length
+        assert x.shape[1] == self.short_ctx_len
+
         qkv_merged: torch.Tensor = self.make_qkv(x)
         # extract q/k/v from merged
         # (batch, seq, n_heads, n_embed)
@@ -137,9 +193,48 @@ class InputLayer(nn.Module):
         k = qkv_merged[..., 1, :, :].transpose(1, 2)
         v = qkv_merged[..., 2, :, :].transpose(1, 2)
 
-        # ??????
+        # apply rotary positional embedding on q/k
+        q = self.rope(q)
+        k = self.rope(k)
+
         # pylint: disable-next=not-callable
         attn_out = F.scaled_dot_product_attention(q, k, v, dropout_p=self.attn_dropout_p)
 
         # transpose back (batch, seq, n_heads, n_embed), then ff
+        # out shape is (batch, seq, n_embed)
         return self.feedforward(attn_out.transpose(1, 2))
+
+class IntermediateLayer(nn.Module):
+    "The intermediate layers(s) of the model"
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.norm = nn.LayerNorm((config.n_embed,))
+        self.attention = PartialCrossAttention(config)
+        self.feedforward = GatedFeedForward(config)
+
+    def forward(self, recurrent: torch.Tensor, internal: torch.Tensor):
+        bypass = internal
+        internal = self.norm(internal)
+        internal = self.attention(recurrent, internal)
+        internal = self.feedforward(internal)
+        return bypass + internal
+
+class OutputLayer(nn.Module):
+    "The output layer of the model"
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.attention = PartialCrossAttention(config)
+        self.feedforward = GatedFeedForward(config)
+
+    def forward(self, recurrent: torch.Tensor, internal: torch.Tensor):
+        # we are computing residual to recurrent state, so "recurrent" is "internal"
+        out = self.attention(internal, recurrent)
+        out = self.feedforward(out)
+        return out
+
+class RNNDerp(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
