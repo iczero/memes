ecadd38e2cb4e5c117e3097517236c92bb56f062
iczero <iczero4@gmail.com>
2024-04-01 22:56:43 -0700

    keep rotary encoding at full precision

--- rnn/rotary_encoding.py
+++ rnn/rotary_encoding.py
@@ -9,24 +9,32 @@ from torch import nn
 # (batch, n_heads, seq, qkv_dim)
 
 BASE = 10000
+ROPE_DTYPE = torch.float32
 
 class RotaryEncoding(nn.Module):
     "Implements rotary positional encoding (RoPE)"
 
     def __init__(self, hidden_dim: int, seq_len: int):
         super().__init__()
-        assert hidden_dim % 2 == 0
-        # compute coefficients at full precision
-        dim_idx = torch.arange(0, hidden_dim, 2, dtype=torch.float64)
-        theta = BASE ** (-dim_idx / hidden_dim)
-        base = theta.unsqueeze(0) * torch.arange(seq_len).type_as(theta).unsqueeze(-1)
-        self.register_buffer('sin_cached', base.sin())
-        self.register_buffer('cos_cached', base.cos())
+        self.cos_cached, self.sin_cached = rope_make_cached(hidden_dim, seq_len)
 
     def forward(self, x):
+        # ensure cached values are on the right device
+        if self.cos_cached.device != x.device:
+            self.cos_cached = self.cos_cached.to(x.device)
+        if self.sin_cached.device != x.device:
+            self.sin_cached = self.sin_cached.to(x.device)
+
         return rotary_encoding_forward(self.cos_cached, self.sin_cached, x)
 
-#@torch.jit.script
+def rope_make_cached(hidden_dim: int, seq_len: int):
+    assert hidden_dim % 2 == 0
+    # compute coefficients at full precision
+    dim_idx = torch.arange(0, hidden_dim, 2, dtype=torch.float64)
+    theta = BASE ** (-dim_idx / hidden_dim)
+    base = theta.unsqueeze(0) * torch.arange(seq_len).type_as(theta).unsqueeze(-1)
+    return base.cos().type(ROPE_DTYPE), base.sin().type(ROPE_DTYPE)
+
 def rotary_encoding_forward(cos_cached: torch.Tensor, sin_cached: torch.Tensor, x: torch.Tensor):
     # [[ cos(m*t), -sin(m*t) ],  @  [[a],
     #  [ sin(m*t),  cos(m*t) ]]      [b]]
@@ -34,8 +42,10 @@ def rotary_encoding_forward(cos_cached: torch.Tensor, sin_cached: torch.Tensor, 
     # b2: sin(m*t) * a + cos(m*t) *  b
 
     # in: (batch..., seq, n_embed)
-    a = x[..., 0::2]
-    b = x[..., 1::2]
+    # maintain full precision
+    x_dt = x.type(ROPE_DTYPE)
+    a = x_dt[..., 0::2]
+    b = x_dt[..., 1::2]
     a2 = cos_cached * a + sin_cached * -b
     b2 = sin_cached * a + cos_cached * b
-    return torch.stack((a2, b2), dim=-1).flatten(-2, -1)
+    return torch.stack((a2, b2), dim=-1).flatten(-2, -1).type_as(x)
